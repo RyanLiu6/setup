9caec9d7001d374861a876885b956bb971e7e6f6
Ryan Liu <dio.ryanliu@hotmail.com>
2019-01-16 22:25:19 -0800

    Updating script to automatically move items

--- Atom/setup.sh
+++ Atom/setup.sh
@@ -1,2 +1,13 @@
+# Installing the theme I prefer
 apm install bliss-ui
 apm install bliss-syntax
+
+# Automatically moving the correct file for you, assuming default installation
+cp config.cson ~/.atom/config.cson
+cp styles.less ~/.atom/styles.less
+
+if [[ "OSTYPE" == "darwin"* ]]; then
+	cp keymap.cson.mac ~/.atom/keymap.cson
+else
+	cp keymap.cson.other ~/.atom/keymap.cson
+fi
