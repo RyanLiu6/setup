1f32336d3d6e3a9933d07174bbc9f65b7e9120d7
Ryan Liu <ryanl@athinkingape.com>
2019-05-13 09:51:04 -0700

    Updating Terminal to alias atom -> code (accepting the dark side)

--- Terminal/setup.sh
+++ Terminal/setup.sh
@@ -3,7 +3,7 @@
 # Automatically moving the correct file for you, assuming default installation
 if [[ "OSTYPE" == "darwin"* ]]; then
     sudo brew install bash-completion
-	cp .bash ~/.bash_profile
+	cp bash_settings ~/.bash_profile
 else
-	cp .bash ~/.bashrc
+	cp bash_settings ~/.bashrc
 fi
